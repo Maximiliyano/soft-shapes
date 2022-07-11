4dee704c1920fd846a1d62f23095ee4790fb61d4
Максим Василишин <maxim24152771@gmail.com>
2022-07-11 16:09:19 +0300

    Updated function

--- Program.cs
+++ Program.cs
@@ -3,55 +3,94 @@ using System;
 
 class Program
 {
+    abstract class Shape : IComparable<Shape>
+    {
+        public abstract double CalcArea();
+
+        public int CompareTo(Shape other)
+        {
+            double area = CalcArea();
+            return area.CompareTo(other.CalcArea());
+        }
+    }
     static void Main(string[] args)
     {
-        Program a = new Program();
-        a.Square();
-        a.Rectangle();
-        a.Triangle();
-        a.Circle();
-        
-        /*
-        var shapes = new List<Shape>{ 
-            new Square(side),
-            new Circle(radius),
-            new Triangle(base, height) };
+        var shapes = new List<Shape>{ new Square(6), new Circle(2), new Triangle(4, 5), new Rectangle(4, 5) };
         shapes.Sort();
-         */
 
-        Console.ReadKey();
+        foreach (var shape in shapes)
+            Console.WriteLine(shape + "\n");
     }
 
-    public void Square()
+    class Square : Shape
     {
-        Console.Write("Enter side Square: ");
-        float side = float.Parse(Console.ReadLine());
-        Console.WriteLine("Area of Square is: {0}", (float)Math.Pow(side, 2));
+        private double Height;
+
+        public Square(double side)
+        {
+            Height = side;
+        }
+
+        public override double CalcArea()
+        {
+            return Math.Pow(Height, 2);
+        }
+        public override string ToString()
+            => $"[Square]\nHeight: {Height}\nArea: {CalcArea()}";
     }
 
-    public void Rectangle()
+    class Rectangle : Shape
     {
-        Console.Write("Enter length Rectangle: ");  
-        float length = float.Parse(Console.ReadLine());  
-        Console.Write("Enter breadth Rectangle: ");  
-        float breadth = float.Parse(Console.ReadLine());
-        Console.WriteLine("Area of Rectangle is: {0}", length * breadth);  
+        private double Length, Breadth;
+
+        public Rectangle(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public override double CalcArea()
+        {
+            return Length * Breadth;
+        }
+        public override string ToString()
+            => $"[Rectangle]\nLength: {Length}\nBreadth: {Breadth}\nArea: {CalcArea()}";
     }
 
-    public void Triangle()
+    class Triangle : Shape
     {
-        Console.Write("Enter breadth Triangle: ");  
-        float bredth = float.Parse(Console.ReadLine());  
-        Console.Write("Enter height Triangle: ");  
-        float height = float.Parse(Console.ReadLine());
-        Console.WriteLine("Area of Triangle is: {0}", (bredth * height) / 2);  
+        private double Bredth, Height;
+
+        public Triangle(double bredth, double height)
+        {
+            Height = height;
+            Bredth = bredth;
+        }
+
+        public override double CalcArea()
+        {
+            return (Bredth * Height) / 2;
+        }
+
+        public override string ToString()
+            => $"[Triangle]\nBase: {Bredth}\nHeight: {Height}\nArea: {CalcArea()}";
     }
     
-    public void Circle()
+    class Circle : Shape
     {
-        Console.Write("Enter radius of Circle: ");  
-        float radius = float.Parse(Console.ReadLine());
-        Console.WriteLine("Area of Circle is: {0}", Math.PI * (float)Math.Pow(radius, 2));  
+        private double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalcArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override string ToString()
+            => $"[Circle]\nRadius: {Radius}\nArea: {CalcArea()}";
     }
     
 }
